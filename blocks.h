5accfe84b0302ab33272b589114ae46a15d9b799
Ahmed Salem <aieisalem17@gmail.com>
2020-06-29 16:15:36 +0000

    Use new path for executables

--- blocks.h
+++ blocks.h
@@ -1,21 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sh ~/.config/scripts/wm/mpvstatus.sh  web-media" , 0, 22},
+	{"", "mpvstatus  web-media" , 0, 22},
 
-	{"", "sh ~/.config/scripts/wm/mpvstatus.sh 🎵 mpv", 0, 23},
+	{"", "mpvstatus 🎵 mpv", 0, 23},
 
 	{"🌡", "sensors | awk '/^CPUTIN:/{print $2}'",	5, 1},
 
 
-	{"", "sh ~/.config/scripts/wm/storage.sh",	30,	2},
+	{"", "storage",	30,	2},
 
 	
-	{"", "sh ~/.config/scripts/wm/mem.sh",	30,		3},
+	{"", "mem",	30,		3},
 
-	{"", "sh ~/.config/scripts/wm/vol.sh", 0, 10},
+	{"", "vol", 0, 10},
 
-	{"", "sh ~/.config/scripts/wm/dtime.sh",	30,		4},
+	{"", "dtime",	30,		4},
 	
 
 };
